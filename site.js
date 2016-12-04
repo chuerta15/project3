940f57359cf229b1a075404cf3f11a3126c6105d
Chip Pickett <rpickett@hawk.iit.edu>
2016-12-03 23:23:47 -0600

    started using gmaps.js to help access google maps;  changed the way we create maps; added a marker for our first position

--- site.js
+++ site.js
@@ -1,18 +1,49 @@
 
-$(document).ready(function() {
-  var longitude;
-  var latitude
 
-  $('#zip-form').on('submit', function(event) {
-    var query = $('#zip-code').val();
-    // $('#test').append(query);
+$(document).ready(function() {
+  var initLat = 41.8403395;
+  var initLong = -87.627092;
+  var map = new GMaps({
+    el: '#map',
+    lat: initLat,
+    lng: initLong,
+  })
+  map.addMarker({
+    lat: initLat,
+    lng: initLong,
+    title: 'your place',
+    click: function(e) {
+      alert('you clicked');
+    }
+  })
+
+$('#zip-form').on('submit', function(event) {
+  var query = $('#zip-code').val();
+    $('#test').append(query);
     $.get(
-      'https://maps.googleapis.com/maps/api/geocode/json?address=' + query + '&key=AIzaSyBscMaTy7jt6fISLrMwGIejOy-1i-BqJ_g',
+      'https://maps.googleapis.com/maps/api/geocode/json?address=' + query + '&key=AIzaSyAz2_Ve_tK4VvHl-F0nD1oKACoQdiw_MZA',
       function(data) {
-        longitude = data.results[0].geometry.location.lng;
-        latitude = data.results[0].geometry.location.lat;
+        $('#test').append(data.results[0]);
+        var longitude = data.results[0].geometry.location.lng;
+        var latitude = data.results[0].geometry.location.lat;
         $('#test').append(' Longitude: ' + longitude + ', Latitude: ' + latitude);
+        map = new GMaps({
+          el: '#map',
+          lat: latitude,
+          lng: longitude,
+        })
+        map.addMarker({
+          lat: latitude,
+          lng: longitude,
+          title: 'your place',
+          click: function(e) {
+            alert('you clicked');
+          }
+        })
       });
+
+
+
     event.preventDefault();
   });
 
